94966d9483ddc5e0e039c02b7085123baac45d33
Andy Whitfield <andy@andywhitfield.me.uk>
2019-12-23 10:59:23 +0000

    Use windows service extension

--- SmallRss.Service/Program.cs
+++ SmallRss.Service/Program.cs
@@ -1,18 +1,23 @@
-﻿using Microsoft.AspNetCore;
+﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Hosting;
 
 namespace SmallRss.Service
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            var host = WebHost.CreateDefaultBuilder(args)
-                .UseKestrel()
-                .UseStartup<Startup>()
+            var host = Host
+                .CreateDefaultBuilder(args)
+                .UseWindowsService()
+                .ConfigureWebHostDefaults(webBuilder => webBuilder
+                    .UseKestrel()
+                    .UseStartup<Startup>()
+                )
                 .Build();
 
-            host.Run();
+            await host.RunAsync();
         }
     }
 }
