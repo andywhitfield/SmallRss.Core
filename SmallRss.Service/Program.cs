d1d68ae2b785238e9b1005af01b58d6062687ebb
Andy Whitfield <andy@andywhitfield.me.uk>
2019-12-31 07:02:02 +0000

    Fix for log path when running in a windows service

--- SmallRss.Service/Program.cs
+++ SmallRss.Service/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Hosting.WindowsServices;
 using Serilog;
 using Serilog.Events;
 
@@ -11,13 +13,18 @@ namespace SmallRss.Service
     {
         public static async Task Main(string[] args)
         {
+            var logPath = ".";
+            if (WindowsServiceHelpers.IsWindowsService())
+                logPath = AppContext.BaseDirectory;
+            logPath = Path.Combine(logPath, "smallrss.service.log");
+
             const string logOutputTemplate = "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] [{SourceContext}] {Message:lj}{NewLine}{Exception}";
             Log.Logger = new LoggerConfiguration()
                 .MinimumLevel.Verbose()
                 .MinimumLevel.Override("Microsoft", LogEventLevel.Information)
                 .Enrich.FromLogContext()
                 .WriteTo.Console(outputTemplate: logOutputTemplate)
-                .WriteTo.File(@"smallrss.service.log", LogEventLevel.Verbose, outputTemplate: logOutputTemplate,
+                .WriteTo.File(logPath, LogEventLevel.Verbose, outputTemplate: logOutputTemplate,
                     fileSizeLimitBytes: 10_000_000, rollOnFileSizeLimit: true, shared: true, flushToDiskInterval: TimeSpan.FromSeconds(1))
                 .CreateLogger();
 
