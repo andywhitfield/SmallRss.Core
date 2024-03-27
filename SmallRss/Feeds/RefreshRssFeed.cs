3c3693a2618eef51b15ef58c460e4e24e93ba4d6
Andy Whitfield <andy@andywhitfield.me.uk>
2024-03-27 19:01:21 +0000

    Fix for feeds where the guid (uri) is updated with a version

--- SmallRss/Feeds/RefreshRssFeed.cs
+++ SmallRss/Feeds/RefreshRssFeed.cs
@@ -7,87 +7,97 @@ using Microsoft.Extensions.Logging;
 using SmallRss.Data;
 using SmallRss.Models;
 
-namespace SmallRss.Feeds
+namespace SmallRss.Feeds;
+
+public class RefreshRssFeed(ILogger<RefreshRssFeed> logger, IHttpClientFactory clientFactory, IFeedParser feedParser,
+    IArticleRepository articleRepository)
+    : IRefreshRssFeed
 {
-    public class RefreshRssFeed : IRefreshRssFeed
+    public async Task<bool> RefreshAsync(RssFeed rssFeed, CancellationToken cancellationToken)
     {
-        private readonly ILogger<RefreshRssFeed> _logger;
-        private readonly IHttpClientFactory _clientFactory;
-        private readonly IFeedParser _feedParser;
-        private readonly IArticleRepository _articleRepository;
-
-        public RefreshRssFeed(ILogger<RefreshRssFeed> logger, IHttpClientFactory clientFactory, IFeedParser feedParser,
-            IArticleRepository articleRepository)
+        using var client = clientFactory.CreateClient(RefreshRssFeedsServiceProviderExtensions.DefaultHttpClient);
+        using var response = await client.GetAsync(rssFeed.Uri, cancellationToken);
+        if (!response.IsSuccessStatusCode)
         {
-            _logger = logger;
-            _clientFactory = clientFactory;
-            _feedParser = feedParser;
-            _articleRepository = articleRepository;
-        }
-
-        public async Task<bool> RefreshAsync(RssFeed rssFeed, CancellationToken cancellationToken)
-        {
-            using var client = _clientFactory.CreateClient(RefreshRssFeedsServiceProviderExtensions.DefaultHttpClient);
-            using var response = await client.GetAsync(rssFeed.Uri, cancellationToken);
-            if (!response.IsSuccessStatusCode)
-            {
-                _logger.LogWarning($"Could not refresh feed {rssFeed.Id} from {rssFeed.Uri}: response status: {response.StatusCode}, content: {await response.Content.ReadAsStringAsync()}");
-                return false;
-            }
-
-            _logger.LogInformation($"Successfully download feed from {rssFeed.Uri}");
-            FeedParseResult parseResult;
-            if (!((parseResult = await _feedParser.ParseAsync(await response.Content.ReadAsStreamAsync(), cancellationToken))?.IsValid ?? false))
-            {
-                _logger.LogWarning($"Could not parse feed response from {rssFeed.Uri} - content: {await response.Content.ReadAsStringAsync()}");
-                return false;
-            }
-
-            _logger.LogDebug($"Feed {rssFeed.Uri} was last updated {parseResult.Feed.LastUpdated} - our version was updated: {rssFeed.LastUpdated}");
-            if (!rssFeed.LastUpdated.HasValue || parseResult.Feed.LastUpdated > rssFeed.LastUpdated)
-            {
-                _logger.LogTrace($"Feed {rssFeed.Uri} has new items...updating articles");
-                await UpdateFeedItemsAsync(rssFeed, parseResult);
-                rssFeed.LastUpdated = parseResult.Feed.LastUpdated;
-                rssFeed.Link = parseResult.Feed.Link;
-                return true;
-            }
-
+            logger.LogWarning($"Could not refresh feed {rssFeed.Id} from {rssFeed.Uri}: response status: {response.StatusCode}, content: {await response.Content.ReadAsStringAsync()}");
             return false;
         }
 
-        private async Task UpdateFeedItemsAsync(RssFeed rssFeed, FeedParseResult parseResult)
+        logger.LogInformation($"Successfully download feed from {rssFeed.Uri}");
+        FeedParseResult parseResult;
+        if (!((parseResult = await feedParser.ParseAsync(await response.Content.ReadAsStreamAsync(), cancellationToken))?.IsValid ?? false))
         {
-            var existing = await _articleRepository.GetByRssFeedIdAsync(rssFeed.Id);
-            var oldestItem = (existing?.Any() ?? false) ? existing.Min(a => a.Published ?? DateTime.MinValue) : DateTime.MinValue;
+            logger.LogWarning($"Could not parse feed response from {rssFeed.Uri} - content: {await response.Content.ReadAsStringAsync()}");
+            return false;
+        }
 
-            foreach (var itemInFeed in parseResult.Articles)
+        logger.LogDebug($"Feed {rssFeed.Uri} was last updated {parseResult.Feed.LastUpdated} - our version was updated: {rssFeed.LastUpdated}");
+        if (!rssFeed.LastUpdated.HasValue || parseResult.Feed.LastUpdated > rssFeed.LastUpdated)
+        {
+            logger.LogTrace($"Feed {rssFeed.Uri} has new items...updating articles");
+            await UpdateFeedItemsAsync(rssFeed, parseResult);
+            rssFeed.LastUpdated = parseResult.Feed.LastUpdated;
+            rssFeed.Link = parseResult.Feed.Link;
+            return true;
+        }
+
+        return false;
+    }
+
+    private async Task UpdateFeedItemsAsync(RssFeed rssFeed, FeedParseResult parseResult)
+    {
+        var existing = await articleRepository.GetByRssFeedIdAsync(rssFeed.Id);
+        var oldestItem = (existing?.Any() ?? false) ? existing.Min(a => a.Published ?? DateTime.MinValue) : DateTime.MinValue;
+
+        foreach (var itemInFeed in parseResult.Articles)
+        {
+            var existingArticle = existing?.FirstOrDefault(e => IsArticleGuidMatch(e.ArticleGuid, itemInFeed.ArticleGuid));
+            if (existingArticle != null)
             {
-                var existingArticle = existing?.FirstOrDefault(e => e.ArticleGuid == itemInFeed.ArticleGuid);
-                if (existingArticle != null)
+                if (itemInFeed.Published > existingArticle.Published)
                 {
-                    if (itemInFeed.Published > existingArticle.Published)
-                    {
-                        _logger.LogInformation($"Article {existingArticle.ArticleGuid} has updated ({itemInFeed.Published} - our version {existingArticle.Published}), updating our instance");
-                        existingArticle.Heading = itemInFeed.Heading;
-                        existingArticle.Body = itemInFeed.Body;
-                        existingArticle.Url = itemInFeed.Url;
-                        existingArticle.Published = itemInFeed.Published;
-                        existingArticle.Author = itemInFeed.Author;
-                    }
-                }
-                else
-                {
-                    _logger.LogInformation($"Add new article {itemInFeed.ArticleGuid}|{itemInFeed.Heading} to feed {rssFeed.Uri}");
-                    if (itemInFeed.Published < oldestItem)
-                    {
-                        _logger.LogWarning($"Article {itemInFeed.ArticleGuid} is older than the oldest item...has probably already been archived so not adding.");
-                        continue;
-                    }
-
-                    await _articleRepository.CreateAsync(rssFeed, itemInFeed);
+                    logger.LogInformation($"Article {existingArticle.ArticleGuid} has updated ({itemInFeed.Published} - our version {existingArticle.Published}), updating our instance");
+                    existingArticle.Heading = itemInFeed.Heading;
+                    existingArticle.Body = itemInFeed.Body;
+                    existingArticle.Url = itemInFeed.Url;
+                    existingArticle.Published = itemInFeed.Published;
+                    existingArticle.Author = itemInFeed.Author;
                 }
             }
+            else
+            {
+                logger.LogInformation($"Add new article {itemInFeed.ArticleGuid}|{itemInFeed.Heading} to feed {rssFeed.Uri}");
+                if (itemInFeed.Published < oldestItem)
+                {
+                    logger.LogWarning($"Article {itemInFeed.ArticleGuid} is older than the oldest item...has probably already been archived so not adding.");
+                    continue;
+                }
+
+                await articleRepository.CreateAsync(rssFeed, itemInFeed);
+            }
+        }
+    }
+
+    private static bool IsArticleGuidMatch(string? articleGuid1, string? articleGuid2)
+    {
+        if (articleGuid1 == articleGuid2)
+            return true;
+
+        if (string.Equals(StripFragment(articleGuid1), StripFragment(articleGuid2), StringComparison.OrdinalIgnoreCase))
+            return true;
+
+        return false;
+
+        static string StripFragment(string? uri)
+        {
+            if (uri == null || !Uri.TryCreate(uri, UriKind.RelativeOrAbsolute, out var _))
+                return uri ?? "";
+
+            uri = uri.Trim();
+            var fragmentIdx = uri.LastIndexOf('#');
+            if (fragmentIdx > 0)
+                return uri[..fragmentIdx];
+            return uri;
         }
     }
 }
